683e7b8c3ae61c243a3073c3f095a013bf4321b6
Jody <jodywhitis0407@gmail.com>
2020-10-07 22:19:43 -0500

    Adding Chart.js to /Charts page.

---
+++ ICUScoreWeb/ICUScore.Web/Controllers/ChartController.cs
@@ -0,0 +1,112 @@
+﻿using ICUScore.Data.Models;
+using ICUScore.Data.Services;
+using ICUScore.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICUScore.Web.Controllers
+{
+    public class ChartController : Controller
+    {
+        InMemoryHighscoreTable hsTable;
+        InMemoryPlayerTable pTable;
+
+        public ChartController(InMemoryPlayerTable inMemoryPlayerTable, InMemoryHighscoreTable inMemoryHighscoreTable)
+        {
+            this.hsTable = inMemoryHighscoreTable;
+            this.pTable = inMemoryPlayerTable;
+        }
+
+        // GET: Chart
+        public ActionResult Index()
+        {
+            IEnumerable<HighScore> highScores = new List<HighScore>();
+            IEnumerable<Player> players = new List<Player>();
+            List<object> idata = new List<object>();
+
+            highScores = hsTable.GetAll();
+            players = pTable.GetAll();
+
+            IEnumerable<ChartViewModel> scoreBoard = (from h in highScores
+                                                      join p in players on h.pID equals p.ID
+                                                      select new ChartViewModel
+                                                      {
+                                                          Name = p.Name,
+                                                          Highscore = h.Highscore
+                                                      });
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name", System.Type.GetType("System.String"));
+            dt.Columns.Add("Score", System.Type.GetType("System.Int32"));
+
+            foreach(var s in scoreBoard)
+            {
+            DataRow dr = dt.NewRow();
+
+            dr["Name"] = s.Name;
+            dr["Score"] = s.Highscore;
+            dt.Rows.Add(dr);
+            }
+            
+
+
+            foreach (DataColumn dc in dt.Columns)
+            {
+                List<object> x = new List<object>();
+                x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
+                idata.Add(x);
+            }
+
+            ViewBag.ChartData = idata.ToArray();
+            return View("~/Views/Chart/Chart.cshtml");
+        }
+
+        [HttpPost]
+        public JsonResult NewChart()
+        {
+            IEnumerable<HighScore> highScores = new List<HighScore>();
+            IEnumerable<Player> players = new List<Player>();
+            List<object> idata = new List<object>();
+
+            highScores = hsTable.GetAll();
+            players = pTable.GetAll();
+
+            IEnumerable<ChartViewModel> scoreBoard = (from h in highScores
+                                                      join p in players on h.pID equals p.ID
+                                                      select new ChartViewModel
+                                                      {
+                                                          Name = p.Name,
+                                                          Highscore = h.Highscore
+                                                      });
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name", System.Type.GetType("System.String"));
+            dt.Columns.Add("Score", System.Type.GetType("System.Int32"));
+            DataRow dr = dt.NewRow();
+
+
+            dr["Name"] = scoreBoard.ToList().First().Name;
+            dr["Score"] = scoreBoard.ToList().First().Highscore;
+            dt.Rows.Add(dr);
+
+
+            foreach (DataColumn dc in dt.Columns)
+            {
+                List<object> x = new List<object>();
+                x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
+                idata.Add(x);
+            }
+
+            ViewBag.ChartData = idata.ToArray();
+            return Json(idata, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+
+
+}
---
+++ ICUScoreWeb/ICUScore.Web/Models/ChartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICUScore.Web.Models
+{
+    public class ChartViewModel
+    {
+        public string Name { get; set; }
+        public double Highscore { get; set; }
+
+    }
+}
